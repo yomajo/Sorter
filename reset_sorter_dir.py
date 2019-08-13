2bc4f8f8aff9826d9db48d5c09dd20890189387b
yomajo <ed.vinas@yahoo.com>
2019-08-13 12:06:40 +0300

    close #1 -added script that resets working directory

---
+++ reset_sorter_dir.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import shutil
+import stat
+import logging
+
+#GLOBAL VARIABLES
+src = os.path.join(os.getcwd(), 'mess backup', 'mess inside')
+dst = os.path.join(os.getcwd(), 'mess inside')
+
+#LOGGING SETTINGS:
+logging.basicConfig(level=logging.INFO)
+
+def on_rm_error(func, path, exc_info):
+    '''handle read-only files while performing rmtree function'''
+    os.chmod(path, stat.S_IWRITE)
+    os.remove(path)
+    
+def reset_messy_dir():
+    '''deletes working folder and recreates it from backup folder'''
+
+    if os.path.exists(dst) == True:
+        logging.info(f'path {dst} exists and is being removed')
+        shutil.rmtree(dst, ignore_errors=False, onerror=on_rm_error)
+
+    shutil.copytree(src, dst)
+    logging.info(f'Directory copied from: {src}\nto: {dst}')
+    logging.info('Finished')
+
+if __name__ == '__main__':
+    reset_messy_dir()
